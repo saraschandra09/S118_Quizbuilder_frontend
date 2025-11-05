c77c7b62a96e0819d590d335a6891af78a5e9838
saraschandra09 <2300031205cseh@gmail.com>
2025-11-05 11:34:35 +0530

    first

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/frontend/', // 👈 Match your Tomcat context path exactly
+  build: {
+    outDir: 'dist',
+  },
 })
